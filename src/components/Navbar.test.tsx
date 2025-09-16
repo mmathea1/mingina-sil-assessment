994d878674b95dbb3156f1fceb6f737d2bf70d96
Mingina <mingina.mathea075@gmail.com>
2025-09-16 16:20:56 +0300

    test: passing test, clicking logo lands to home page

--- src/components/Navbar.test.tsx
+++ src/components/Navbar.test.tsx
@@ -2,21 +2,22 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import Navbar from "./Navbar";
-import { usePathname } from "next/navigation";
 
 jest.mock("next/navigation", () => ({
-  usePathname: jest.fn(),
+  useRouter: () => ({
+    push: jest.fn(),
+    prefetch: jest.fn(),
+  }),
 }));
 
 describe("Navbar", () => {
-  it("renders brand button", () => {
+  it("renders logo button", () => {
     render(<Navbar />);
     expect(screen.getByText("Ming Movies")).toBeInTheDocument();
   });
 
   it("navigates to home page when logo is clicked", async () => {
     const user = userEvent.setup();
-    (usePathname as jest.Mock).mockReturnValue("/search");
 
     render(<Navbar />);
     const logo = screen.getByText("Ming Movies");
