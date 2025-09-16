d4c2afea809330deb58bf14381219273455d4315
Mingina <mingina.mathea075@gmail.com>
2025-09-16 21:29:09 +0300

    test: testing movie title renders in card info and overview

---
+++ src/components/MovieCrad.test.tsx
@@ -0,0 +1,21 @@
+import { Movie } from "@/types/interfaces";
+import { render, screen } from "@testing-library/react";
+import MovieCard from "./MovieCard";
+
+const mockMovie: Movie = {
+  id: "2345356",
+  title: "Inception",
+  poster_path: "/poster.jpg",
+  overview: "A mind bending thriller",
+  vote_average: 8.6,
+  release_date: "2010-07-16",
+  rating: null,
+};
+
+describe("MovieCard", () => {
+  it("renders movie title in both info and overview", () => {
+    render(<MovieCard movie={mockMovie} />);
+    const titles = screen.getAllByText("Inception");
+    expect(titles).toHaveLength(2);
+  });
+});
