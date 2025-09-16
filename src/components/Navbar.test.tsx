9f8b191b0f5ec586888b2820835e423a350b6f05
Mingina <mingina.mathea075@gmail.com>
2025-09-16 16:04:46 +0300

    test: testing logo navigates to home page when clicked

--- src/components/Navbar.test.tsx
+++ src/components/Navbar.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import Navbar from "./Navbar";
+import { usePathname } from "next/navigation";
 
 jest.mock("next/navigation", () => ({
   usePathname: jest.fn(),
@@ -10,5 +12,17 @@ describe("Navbar", () => {
   it("renders brand button", () => {
     render(<Navbar />);
     expect(screen.getByText("Ming Movies")).toBeInTheDocument();
+  });
+
+  it("navigates to home page when logo is clicked", async () => {
+    const user = userEvent.setup();
+    (usePathname as jest.Mock).mockReturnValue("/search");
+
+    render(<Navbar />);
+    const logo = screen.getByText("Ming Movies");
+
+    await user.click(logo);
+
+    expect(logo.closest("a")).toHaveAttribute("href", "/");
   });
 });
