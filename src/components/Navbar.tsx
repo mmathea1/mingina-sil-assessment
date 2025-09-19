d7dc3775d53e2af4f6cb1c70d7938be100f2c5b8
Mingina Mathea <3862039-mmathea1@users.noreply.gitlab.com>
2025-09-19 15:31:33 +0300

    style: improving navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
         </div>
         <div className="navbar-center hidden md:flex flex-none flex items-center gap-4">
           <form className="form-control flex items-center w-full max-w-sm" onSubmit={handleSearch}>
-            <label className="input input-bordered flex items-center gap-2 !rounded-full w-64 md:w-80 hover:border-blue-500 focus:outline-none focus:ring-0 focus-within:border-blue-500 focus-within:shadow-none transition-colors duration-200 group">
+            <label className="input flex items-center gap-2 !rounded-full w-64 md:w-80 hover:bg-blue-100 focus:outline-none focus:ring-0 focus-within:border-blue-200 focus-within:shadow-none transition-colors duration-200 group">
               <Search className="w-5 h-5 opacity-50 group-focus-within:text-blue-500" />
               <input
                 type="text"
@@ -41,14 +41,14 @@ export default function Navbar() {
         <div className="flex items-center gap-2 navbar-end">
           <button className="btn btn-ghost md:hidden">☰</button>
           <div className="hidden md:flex gap-2">
-            <button className="btn btn-ghost btn-circle">
-              <Mail className="w-5 h-5 text-white-500 hover:text-blue-500 transition-colors" />
+            <button className="btn btn-ghost btn-circle hover:bg-blue-100 group">
+              <Mail className="ww-5 h-5 text-gray-500 group-hover:text-blue-500 transition-colors" />
             </button>
-            <button className="btn btn-ghost btn-circle">
-              <CircleUser className="w-5 h-5 text-white-500 hover:text-blue-500 transition-colors" />
+            <button className="btn btn-ghost btn-circle hover:bg-blue-100 group">
+              <CircleUser className="w-5 h-5 text-gray-500 group-hover:text-blue-500 transition-colors" />
             </button>
-            <button className="btn btn-ghost btn-circle">
-              <Settings className="w-5 h-5 text-white-500 hover:text-blue-500 transition-colors" />
+            <button className="btn btn-ghost btn-circle hover:bg-blue-100 group">
+              <Settings className="w-5 h-5 text-gray-500 group-hover:text-blue-500 transition-colors" />
             </button>
           </div>
         </div>
