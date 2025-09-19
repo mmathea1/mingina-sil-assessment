1219d2d7f89b5d7e9c0ee359f02374b718f6e633
Mingina Mathea <3862039-mmathea1@users.noreply.gitlab.com>
2025-09-19 14:51:53 +0300

    feat: created app logo component

---
+++ src/components/AppLogo.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Link from "next/link";
+
+export default function AppLogo() {
+  return (
+    <Link href="/" className="btn btn-ghost normal-case text-xl">
+      Ming Movies
+    </Link>
+  );
+}
