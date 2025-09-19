659dc9f441b5bf1663812cbf4567146f5ff2e2ce
Mingina Mathea <3862039-mmathea1@users.noreply.gitlab.com>
2025-09-19 12:41:31 +0300

    style: adding no file found icon

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,7 @@
 import MovieCard from "@/components/MovieCard";
 import { Movie } from "@/types/interfaces";
 import { searchMovies } from "@/utils/api";
+import { FileQuestionMark } from "lucide-react";
 
 export default async function SearchResultsPage({
   searchParams,
@@ -22,20 +23,7 @@ export default async function SearchResultsPage({
         movies.map((movie: Movie) => <MovieCard key={movie.id} movie={movie} />)
       ) : (
         <div className="flex flex-col items-center justify-center mt-10 text-gray-500">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-12 w-12 mb-2 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M9 13h6m2 9H7a2 2 0 01-2-2V5a2 2 0 012-2h4l2 2h6a2 2 0 012 2v12a2 2 0 01-2 2z"
-            />
-          </svg>
+          <FileQuestionMark className="w-24 h-24 text-gray-400 p-2" />
           <p className="text-lg font-medium"> No Results Found For: &quot; {query} &quot; </p>
         </div>
       )}
