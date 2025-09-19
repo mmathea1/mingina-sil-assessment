5c963cd0fd33c2a9a04057392fbe9198593412f4
Mingina Mathea <3862039-mmathea1@users.noreply.gitlab.com>
2025-09-19 18:10:02 +0300

    feat(modal): add modal capability to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import MovieCard from "@/components/MovieCard";
+import MovieDetailModal from "@/components/MovieDetailModal";
 import Pagination from "@/components/Pagination";
 import { fetchMovies } from "@/services/tmdb";
 import { Movie, MovieResponse } from "@/types/interfaces";
@@ -9,11 +10,23 @@ import { useState } from "react";
 
 export default function Home() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { data, isLoading, isError } = useQuery<MovieResponse>({
     queryKey: ["movies", currentPage],
     queryFn: () => fetchMovies(currentPage),
   });
+
+  const handleOpenModal = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+    setIsModalOpen(false);
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error!</div>;
@@ -29,9 +42,11 @@ export default function Home() {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-col-5 gap-6 justify-center">
         {data?.results.map((movie: Movie) => (
-          <MovieCard key={movie.id} movie={movie} />
+          <MovieCard key={movie.id} movie={movie} onClick={handleOpenModal} />
         ))}
       </div>
+
+      <MovieDetailModal movie={selectedMovie} isOpen={isModalOpen} onClose={handleCloseModal} />
     </main>
   );
 }
