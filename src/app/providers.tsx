cc3d7b8d0b0a09ded1947684a0e85255ba5fda20
Mingina Mathea <3862039-mmathea1@users.noreply.gitlab.com>
2025-09-20 16:25:03 +0300

    feat: added next-auth and auth.js file

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,17 +2,22 @@
 
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 2,
       refetchOnWindowFocus: true,
-      staleTime: 1000 * 60 * 10, // 5 minutes
+      staleTime: 1000 * 60 * 10, // 10 minutes
       refetchOnReconnect: "always",
     },
   },
 });
 export function Providers({ children }: { children: ReactNode }) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
+  );
 }
