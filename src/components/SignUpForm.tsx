9e73cd6622326345aa90f0f57dde3011cc967c7d
Mingina Mathea <3862039-mmathea1@users.noreply.gitlab.com>
2025-09-23 19:13:57 +0300

    feat: new signup form

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,21 +1,29 @@
-"use client";
-
-import AppLogo from "./AppLogo";
-import { FormEvent, useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, firebaseErrorMessages } from "@/lib/firebase";
 import { FirebaseError } from "firebase/app";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { FormEvent, useState } from "react";
+import AppLogo from "./AppLogo";
 
-export default function LoginForm() {
+export default function SignUpForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState(" ");
+  const [loading, setLoading] = useState(false);
 
-  const submitLoginForm = async (event: FormEvent<HTMLFormElement>) => {
+  const submitSignUpForm = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setError("null");
+    setLoading(true);
+
     try {
-      await signInWithEmailAndPassword(auth, email, password);
-      console.log("logged in successfully");
+      if (password !== confirmPassword) {
+        setError("Passwords do not match");
+        setLoading(false);
+      } else {
+        await createUserWithEmailAndPassword(auth, email, password);
+        console.log("signed up successfully");
+      }
     } catch (err: unknown) {
       if (err instanceof FirebaseError) {
         const errorMessage =
@@ -24,15 +32,16 @@ export default function LoginForm() {
       } else {
         setError("An unexpected error occured. Please try again.");
       }
+    } finally {
+      setLoading(false);
     }
   };
-
   return (
     <div className="flex flex-col items-center gap-4">
       <AppLogo />
       {error && <p className="text-red-500 text-sm mt-2 font-medium">{error}</p>}
       <form
-        onSubmit={submitLoginForm}
+        onSubmit={submitSignUpForm}
         className="form-control w-full max-w-lg mx-auto p-6 bg-white shadow-md rounded-lg flex flex-col gap-4"
       >
         <div className="mt-4">
@@ -69,15 +78,29 @@ export default function LoginForm() {
             required
           />
         </div>
+        <div className="mt-4">
+          <label className="block pb-2 text-sm font-medium" htmlFor="password">
+            Confirm Password
+          </label>
+          <input
+            className="input input-bordered flex items-center gap-2 rounded-full w-full hover:bg-blue-100 focus:outline-none focus:ring-0 focus-within:border-blue-200 focus-within:shadow-none transition-colors duration-200 group"
+            type="password"
+            value={confirmPassword}
+            name="password"
+            placeholder="Enter Password"
+            id="password"
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
+            required
+          />
+        </div>
         <div className="mt-4 flex flex-col gap-2 items-center">
-          <button type="submit" className="btn btn-primary w-full rounded-full">
-            LOGIN
+          <button type="submit" className="btn btn-primary w-full rounded-full" disabled={loading}>
+            {loading ? "Creating Account..." : "SIGN UP"}
           </button>
           <a href="#" className="text-blue-500 text-sm text-center mt-2 hover:underline">
-            Forgot Password?
-          </a>
-          <a href="/signup" className="text-blue-500 text-sm text-center mt-2 hover:underline">
-            Create An Account?
+            Forgot Password?{" "}
           </a>
         </div>
       </form>
