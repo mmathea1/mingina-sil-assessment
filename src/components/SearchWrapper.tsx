379e6d553d6ea14b36373310819490e141e4e225
Mingina Mathea <3862039-mmathea1@users.noreply.gitlab.com>
2025-09-24 11:23:09 +0300

    fix: fixing no image div, fixing results page grid

--- src/components/SearchWrapper.tsx
+++ src/components/SearchWrapper.tsx
@@ -20,21 +20,22 @@ export default function SearchWrapper({ movies, query }: { movies: Movie[]; quer
     setIsModalOpen(false);
   };
   return (
-    <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-col-5 gap-6 justify-center">
-      {movies.length > 0 ? (
-        movies.map((movie: Movie) => (
-          <MovieCard key={movie.id} movie={movie} onClick={handleOpenModal} />
-        ))
-      ) : (
-        <div className="flex flex-col items-center justify-center mt-16 text-gray-500">
-          <FileQuestionMark className="w-24 h-24 text-gray-400 mb-4" />
-          <p className="text-xl font-medium text-gray-600">
-            No results found for:
-            <span className="text-blue-500"> {query}</span>
-          </p>
-        </div>
-      )}
-
+    <main className="p-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-col-5 gap-6 justify-center">
+        {movies.length > 0 ? (
+          movies.map((movie: Movie) => (
+            <MovieCard key={movie.id} movie={movie} onClick={handleOpenModal} />
+          ))
+        ) : (
+          <div className="flex flex-col items-center justify-center mt-16 text-gray-500">
+            <FileQuestionMark className="w-24 h-24 text-gray-400 mb-4" />
+            <p className="text-xl font-medium text-gray-600">
+              No results found for:
+              <span className="text-blue-500"> {query}</span>
+            </p>
+          </div>
+        )}
+      </div>
       <MovieDetailModal movie={selectedMovie} isOpen={isModalOpen} onClose={handleCloseModal} />
     </main>
   );
